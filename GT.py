4dc528a1ea8021bbb6615f5fc15383901020b772
mikrutm <daneanalzialokalnie@gmail.com>
2023-10-31 13:12:49 +0100

    dsfsdf

--- GT.py
+++ GT.py
@@ -19,14 +19,10 @@ from os.path import dirname, abspath
 import os
 from selenium.webdriver.firefox.options import Options
 
-
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@ st.cache_resource
+@st.cache_resource
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
