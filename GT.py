8b649038f3daad4305c43c5db9acb38f523f5a7e
mikrutm <daneanalzialokalnie@gmail.com>
2023-10-31 13:24:01 +0100

    zdfss

--- GT.py
+++ GT.py
@@ -1,3 +1,33 @@
+with st.echo():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+
+    @st.experimental_singleton
+    def get_driver():
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+
+    driver = get_driver()
+    driver.get("http://example.com")
+
+    st.code(driver.page_source)
+
+
+
+
+
+
+
+
+
+
+
+
 import streamlit  as st
 from selenium import webdriver
 import matplotlib.pyplot as plt
